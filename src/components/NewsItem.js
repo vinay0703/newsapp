e094af4fc06f7a52b2b6ad678e311d04af584850
Araveti Aswartha Narayana Vinay <68630619+vinay0703@users.noreply.github.com>
2022-01-20 16:15:21 +0530

    Added version 1 of class based components

---
+++ src/components/NewsItem.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+import breakingNewsImage from './breakingNewsImage.jpg';
+
+export class NewsItem extends Component {
+    render() {
+        let {title, description, imageUrl, newsUrl, author, date, source} = this.props;
+        return (
+            <div className="my-3">
+                <div className="card">
+                <div style={{display:'flex', justifyContent:'flex-end', position:'absolute', right:'0'}}>
+                <span className="badge rounded-pill bg-danger" style={{left:'90%', zIndex:'1'}}> {source} </span> 
+                </div>
+                    <img src= {imageUrl?imageUrl:breakingNewsImage} className="card-img-top" alt="..."/>
+                    <div className="card-body">
+                        <h5 className="card-title">{title}</h5>
+                        <p className="card-text">{description}</p>
+                        <p className="card-text"><small className="text-muted">By {!author?"Unknown":author} on {new Date(date).toGMTString()}</small></p>
+                        <a href={newsUrl} rel = "noopener noreferrer" target = {'_blank'}className="btn btn-sm btn-dark">Read more</a>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default NewsItem;
